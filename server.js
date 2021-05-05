3dc38b32e343ced844f312c11562183a3aaadbfe
Dawid Bulinski (PGS Software) <dbulinski@pgs-soft.com>
2021-05-05 14:12:23 +0200

    Added date req

--- server.js
+++ server.js
@@ -2,31 +2,44 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+const { isValid } = require("date-fns");
+const express = require("express");
+const app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+const cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: "hello API" });
 });
 
+app.get(
+  "/api/:date?",
+  (req, res, next) => {
+    const date = req.params.date;
+    if (date) {
+      req.date = new Date(date);
+    } else {
+      req.date = new Date();
+    }
+    next();
+  },
+  (req, res) => {
+    if (isValid(req.date)) {
+      res.json({ unix: req.date.getTime(), utc: req.date.toString() });
+    } else {
+      res.json({ error: req.date.toString() });
+    }
+  }
+);
 
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+const listener = app.listen(3000, function () {
+  console.log("Your app is listening on port " + listener.address().port);
 });
